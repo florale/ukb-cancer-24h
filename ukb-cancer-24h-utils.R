2658e5613fca3112aae5d5c350be0613b891fe8e
Flora Le <flora.le@monash.edu>
2024-05-16 13:24:35 +1000

    utils file

---
+++ ukb-cancer-24h-utils.R
@@ -0,0 +1,6 @@
+
+redir <- "/Users/florale/Library/CloudStorage/OneDrive-MonashUniversity/GitHub/projects/ukbiobank/"
+
+# colour pallete
+c("#978787", "#BEACA2", "#EFE3E0", "#A1B2C2", "#8399AE")
+c("#EFE3E0", "#BEACA2", "#708885", "#5A6367")
