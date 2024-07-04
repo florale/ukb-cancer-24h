9d79f916eb2ee3b9fc6ae06283864fc1929698dc
Flora Le <flora.le@monash.edu>
2024-07-04 21:12:00 +1000

    adj quantile plots + minor clean up

--- ukb-cancer-24h-adj-time-quantile.R
+++ ukb-cancer-24h-adj-time-quantile.R
@@ -116,13 +116,21 @@ comp_cancer_time_low_adj[, sig_ref_healthy := ifelse(nonsig_vs_healthy == FALSE 
 comp_cancer_time_low_adj[, nonsig_vs_lag := between(0, comp_cancer_time_low_adj$CI_low_diff_ref_lag, comp_cancer_time_low_adj$CI_high_diff_ref_lag)]
 comp_cancer_time_low_adj[, sig_ref_lag := ifelse(nonsig_vs_lag == FALSE & !is.na(Mean_diff_ref_lag), paste(intToUtf8(8225)), "")] ## double dagger 
 
-
 # sort by time since diagnoses
 comp_cancer_time_low_adj[, cancer_time := factor(cancer_time, ordered = TRUE,
-                                                 levels = c("Healthy",
-                                                            "Less than 1 year since diagnosis",
-                                                            "1-5 years since diagnosis",
-                                                            "More than 5 years since diagnosis"))]
+                                                 levels = c(
+                                                   "Healthy",
+                                                   "Less than 1 year since diagnosis",
+                                                   "1-5 years since diagnosis",
+                                                   "More than 5 years since diagnosis"))]
+comp_cancer_time_low_adj[, cancer_time := factor(cancer_time, ordered = TRUE,
+                                                 levels = c(
+                                                   "More than 5 years since diagnosis",
+                                                   "1-5 years since diagnosis",
+                                                   "Less than 1 year since diagnosis",
+                                                   "Healthy"))]
+
+# add info to make nice plots
 comp_cancer_time_low_adj[, yintercept := NA]
 comp_cancer_time_low_adj[, yintercept := ifelse(part == "sleep", comp_cancer_time_low_adj[cancer_time == "Healthy" & part == "sleep"]$Mean, yintercept)]
 comp_cancer_time_low_adj[, yintercept := ifelse(part == "mvpa", comp_cancer_time_low_adj[cancer_time == "Healthy" & part == "mvpa"]$Mean, yintercept)]
@@ -136,27 +144,32 @@ comp_cancer_time_low_adj[, part := ifelse(part == "sb", "Minutes in Sedentary be
 
 comp_cancer_time_low_adj[, quantile := "Q1"]
 
+# plot
 (plot_comp_cancer_time_low_adj <- 
     ggplot(comp_cancer_time_low_adj, aes(x = cancer_time, y = Mean, group = part)) +
-    geom_hline(aes(yintercept = yintercept), linewidth = 0.2, linetype = 2) +
+    geom_hline(aes(yintercept = yintercept), linewidth = 0.2, linetype = 2, colour = pal_time[1]) +
     geom_pointrange(aes(ymin = CI_low,
-                        ymax = CI_high, colour = part)) +
-    geom_text(aes(label = sig_ref_healthy, colour = part), 
+                        ymax = CI_high, colour = cancer_time)) +
+    geom_text(aes(label = sig_ref_healthy, colour = cancer_time), 
               size = 3, nudge_x = 0.2, nudge_y = 1, 
               show.legend = FALSE) +
-    geom_text(aes(label = sig_ref_lag, colour = part), 
+    geom_text(aes(label = sig_ref_lag, colour = cancer_time), 
               size = 3, nudge_x = 0.2, nudge_y = 1.5, 
               show.legend = FALSE) +
     facet_wrap(~part, scales = "free") +
-    scale_colour_jco() +
-    scale_fill_jco() +
+    scale_colour_manual(values = pal_time) +
+    # scale_colour_jco() +
     labs(x = "", y = "", colour = "") +
     coord_flip() +      
     theme_ipsum() +
     theme(
       axis.ticks        = element_blank(),
       panel.background  = element_rect(fill = "transparent", colour = "black", linewidth = 0.5),
-      legend.position    = "none"
+      plot.background   = element_rect(fill = "transparent", colour = NA),
+      # panel.grid.major  = element_blank(),
+      panel.grid.minor  = element_blank(),
+      strip.text        = element_text(size = 12, hjust = .5, face = "bold"),
+      legend.position   = "none"
     )
 )
 
@@ -274,13 +287,21 @@ comp_cancer_time_mdn_adj[, sig_ref_healthy := ifelse(nonsig_vs_healthy == FALSE 
 comp_cancer_time_mdn_adj[, nonsig_vs_lag := between(0, comp_cancer_time_mdn_adj$CI_low_diff_ref_lag, comp_cancer_time_mdn_adj$CI_high_diff_ref_lag)]
 comp_cancer_time_mdn_adj[, sig_ref_lag := ifelse(nonsig_vs_lag == FALSE & !is.na(Mean_diff_ref_lag), paste(intToUtf8(8225)), "")] ## double dagger 
 
-
 # sort by time since diagnoses
+# comp_cancer_time_mdn_adj[, cancer_time := factor(cancer_time, ordered = TRUE,
+#                                                  levels = c(
+#                                                    "Healthy",
+#                                                    "Less than 1 year since diagnosis",
+#                                                    "1-5 years since diagnosis",
+#                                                    "More than 5 years since diagnosis"))]
 comp_cancer_time_mdn_adj[, cancer_time := factor(cancer_time, ordered = TRUE,
-                                                 levels = c("Healthy",
-                                                            "Less than 1 year since diagnosis",
-                                                            "1-5 years since diagnosis",
-                                                            "More than 5 years since diagnosis"))]
+                                                 levels = c(
+                                                   "More than 5 years since diagnosis",
+                                                   "1-5 years since diagnosis",
+                                                   "Less than 1 year since diagnosis",
+                                                   "Healthy"))]
+
+# add info to make nice plots
 comp_cancer_time_mdn_adj[, yintercept := NA]
 comp_cancer_time_mdn_adj[, yintercept := ifelse(part == "sleep", comp_cancer_time_mdn_adj[cancer_time == "Healthy" & part == "sleep"]$Mean, yintercept)]
 comp_cancer_time_mdn_adj[, yintercept := ifelse(part == "mvpa", comp_cancer_time_mdn_adj[cancer_time == "Healthy" & part == "mvpa"]$Mean, yintercept)]
@@ -294,27 +315,32 @@ comp_cancer_time_mdn_adj[, part := ifelse(part == "sb", "Minutes in Sedentary be
 
 comp_cancer_time_mdn_adj[, quantile := "Q2 and Q3"]
 
+# plot
 (plot_comp_cancer_time_mdn_adj <- 
     ggplot(comp_cancer_time_mdn_adj, aes(x = cancer_time, y = Mean, group = part)) +
     geom_hline(aes(yintercept = yintercept), linewidth = 0.2, linetype = 2) +
     geom_pointrange(aes(ymin = CI_low,
-                        ymax = CI_high, colour = part)) +
-    geom_text(aes(label = sig_ref_healthy, colour = part), 
+                        ymax = CI_high, colour = cancer_time)) +
+    geom_text(aes(label = sig_ref_healthy, colour = cancer_time), 
               size = 3, nudge_x = 0.2, nudge_y = 1, 
               show.legend = FALSE) +
-    geom_text(aes(label = sig_ref_lag, colour = part), 
+    geom_text(aes(label = sig_ref_lag, colour = cancer_time), 
               size = 3, nudge_x = 0.2, nudge_y = 1.5, 
               show.legend = FALSE) +
     facet_wrap(~part, scales = "free") +
-    scale_colour_jco() +
-    scale_fill_jco() +
+    scale_colour_manual(values = pal_time) +
+    # scale_colour_jco() +
     labs(x = "", y = "", colour = "") +
     coord_flip() +      
     theme_ipsum() +
     theme(
       axis.ticks        = element_blank(),
       panel.background  = element_rect(fill = "transparent", colour = "black", linewidth = 0.5),
-      legend.position    = "none"
+      plot.background   = element_rect(fill = "transparent", colour = NA),
+      # panel.grid.major  = element_blank(),
+      panel.grid.minor  = element_blank(),
+      strip.text        = element_text(size = 12, hjust = .5, face = "bold"),
+      legend.position   = "none"
     )
 )
 
@@ -432,13 +458,21 @@ comp_cancer_time_high_adj[, sig_ref_healthy := ifelse(nonsig_vs_healthy == FALSE
 comp_cancer_time_high_adj[, nonsig_vs_lag := between(0, comp_cancer_time_high_adj$CI_low_diff_ref_lag, comp_cancer_time_high_adj$CI_high_diff_ref_lag)]
 comp_cancer_time_high_adj[, sig_ref_lag := ifelse(nonsig_vs_lag == FALSE & !is.na(Mean_diff_ref_lag), paste(intToUtf8(8225)), "")] ## double dagger 
 
-
 # sort by time since diagnoses
+# comp_cancer_time_high_adj[, cancer_time := factor(cancer_time, ordered = TRUE,
+#                                                   levels = c(
+#                                                     "Healthy",
+#                                                     "Less than 1 year since diagnosis",
+#                                                     "1-5 years since diagnosis",
+#                                                     "More than 5 years since diagnosis"))]
 comp_cancer_time_high_adj[, cancer_time := factor(cancer_time, ordered = TRUE,
-                                                  levels = c("Healthy",
-                                                             "Less than 1 year since diagnosis",
-                                                             "1-5 years since diagnosis",
-                                                             "More than 5 years since diagnosis"))]
+                                                  levels = c(
+                                                    "More than 5 years since diagnosis",
+                                                    "1-5 years since diagnosis",
+                                                    "Less than 1 year since diagnosis",
+                                                    "Healthy"))]
+
+# add info to make nice plots
 comp_cancer_time_high_adj[, yintercept := NA]
 comp_cancer_time_high_adj[, yintercept := ifelse(part == "sleep", comp_cancer_time_high_adj[cancer_time == "Healthy" & part == "sleep"]$Mean, yintercept)]
 comp_cancer_time_high_adj[, yintercept := ifelse(part == "mvpa", comp_cancer_time_high_adj[cancer_time == "Healthy" & part == "mvpa"]$Mean, yintercept)]
@@ -452,26 +486,81 @@ comp_cancer_time_high_adj[, part := ifelse(part == "sb", "Minutes in Sedentary b
 
 comp_cancer_time_high_adj[, quantile := "Q4"]
 
+# plot
 (plot_comp_cancer_time_high_adj <- 
     ggplot(comp_cancer_time_high_adj, aes(x = cancer_time, y = Mean, group = part)) +
     geom_hline(aes(yintercept = yintercept), linewidth = 0.2, linetype = 2) +
     geom_pointrange(aes(ymin = CI_low,
-                        ymax = CI_high, colour = part)) +
-    geom_text(aes(label = sig_ref_healthy, colour = part), 
+                        ymax = CI_high, colour = cancer_time)) +
+    geom_text(aes(label = sig_ref_healthy, colour = cancer_time), 
               size = 3, nudge_x = 0.2, nudge_y = 1, 
               show.legend = FALSE) +
-    geom_text(aes(label = sig_ref_lag, colour = part), 
+    geom_text(aes(label = sig_ref_lag, colour = cancer_time), 
               size = 3, nudge_x = 0.2, nudge_y = 1.5, 
               show.legend = FALSE) +
     facet_wrap(~part, scales = "free") +
-    scale_colour_jco() +
-    scale_fill_jco() +
+    scale_colour_manual(values = pal_time) +
+    # scale_colour_jco() +
     labs(x = "", y = "", colour = "") +
     coord_flip() +      
     theme_ipsum() +
     theme(
       axis.ticks        = element_blank(),
       panel.background  = element_rect(fill = "transparent", colour = "black", linewidth = 0.5),
-      legend.position    = "none"
+      plot.background   = element_rect(fill = "transparent", colour = NA),
+      panel.grid.major.x  = element_blank(),
+      panel.grid.minor  = element_blank(),
+      strip.text        = element_text(size = 12, hjust = .5, face = "bold"),
+      legend.position   = "none"
     )
 )
+
+# ALL Qs -----------------------
+comp_cancer_time_low_adj[, quantile := "Q1"]
+comp_cancer_time_mdn_adj[, quantile := "Q2 and Q3"]
+comp_cancer_time_high_adj[, quantile := "Q4"]
+
+comp_cancer_time_quantile_adj <- rbind(comp_cancer_time_low_adj,
+                                       comp_cancer_time_mdn_adj,
+                                       comp_cancer_time_high_adj
+)
+comp_cancer_time_quantile_adj[, text_position := max(CI_high), by = part]
+
+(plot_comp_cancer_time_quantile_adj <- 
+    ggplot(comp_cancer_time_quantile_adj, aes(x = cancer_time, y = Mean, colour = interaction(cancer_time, quantile))) +
+    geom_hline(aes(yintercept = yintercept, linetype = quantile), linewidth = 0.5, colour = "#a8a8a8") +
+    # geom_point(aes(y = Mean, shape = quantile),
+    #           position = position_dodge(width = 1)) +
+    # geom_line( position = position_dodge(width = .75), colour = "#c1c1c1") +
+    geom_pointrange(aes(ymin = CI_low,
+                        ymax = CI_high, shape = quantile, linetype = quantile, size = quantile),
+                    position = position_dodge(width = 1)) +
+    geom_text(aes(y = text_position + 6, label = sig_ref_healthy, colour = cancer_time), 
+              size = 3, 
+              position = position_dodge2(width = 1),
+              show.legend = FALSE) +
+    geom_text(aes(y = text_position + 8, label = sig_ref_lag, colour = cancer_time), 
+              size = 3, 
+              position = position_dodge2(width = 1),
+              show.legend = FALSE) +
+    # geom_label(aes(y = Mean, label = Sig)) + 
+    facet_wrap(~part, scales = "free") +
+    scale_colour_manual(values = pal_time_quantile) +
+    scale_shape_manual(values = c(13, 16, 4)) +
+    scale_size_manual(values = c(.75, .75, .75)) +
+    scale_linetype_manual(values = c("dashed", "solid", "longdash")) +
+    # scale_x_discrete(sec.axis = sec_axis(labels = comp_cancer_24h_type_quantile_adj$Sig)) +
+    labs(x = "", y = "", colour = "") +
+    coord_flip() +
+    theme_ipsum() +
+    theme(
+      axis.ticks        = element_blank(),
+      panel.background  = element_rect(fill = "transparent", colour = "black", linewidth = 0.5),
+      plot.background   = element_rect(fill = "transparent", colour = NA),
+      panel.grid.major.x  = element_blank(),
+      panel.grid.minor  = element_blank(),
+      strip.text        = element_text(size = 12, hjust = .5, face = "bold"),
+      legend.position   = "none"
+    )
+)
+
