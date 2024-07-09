ef0597b5adfc3594362aed20b948e6aa95c18b41
Flora Le <flora.le@monash.edu>
2024-07-09 11:25:51 +1000

    add pairwise contrast within cancer_time group

--- ukb-cancer-24h-adj-time.R
+++ ukb-cancer-24h-adj-time.R
@@ -7,7 +7,7 @@ source(paste0(redir, "ukb_utils.R"))
 #                                mvbind(ilr1, ilr2, ilr3) ~ cancer_time +
 #                                  s(age) + sex + white + working + edu + never_smoked + current_drinker + deprivation,
 #                                # save_pars = save_pars(all = TRUE),
-#                                warmup = 500, chains = 4, cores = 4, backend = "cmdstanr",
+#                                warmup = 500, chains = 4, cores = 4, backend = "cmdstanr"
 # )
 # saveRDS(fit_cancer_time_adj, paste0(outputdir, "fit_cancer_time_adj", ".RDS"))
 
@@ -21,8 +21,8 @@ d_cancer_time_adj <- emmeans::ref_grid(fit_cancer_time_adj$model)@grid
 pred_cancer_time_adj <- fitted(fit_cancer_time_adj, newdata = d_cancer_time_adj, scale = "response", summary = FALSE)
 
 # summarise by cancer types
-pred <- apply(pred_cancer_time_adj, c(1), function(x)  cbind(d_cancer_time_adj, x))
-pred <- lapply(pred, function(d) {
+pred_cancer_time_adj <- apply(pred_cancer_time_adj, c(1), function(x)  cbind(d_cancer_time_adj, x))
+pred_cancer_time_adj <- lapply(pred_cancer_time_adj, function(d) {
   d <- as.data.table(d)
   
   # estimated means by time
@@ -31,32 +31,49 @@ pred <- lapply(pred, function(d) {
   d[, lpa   := mean(V3), by = cancer_time]
   d[, sb    := mean(V4), by = cancer_time]
   
-  # constrastvs healthy
-  d[, sleep_vs_healthy := sleep - d$sleep[1]]
-  d[, mvpa_vs_healthy := mvpa - d$mvpa[1]]
-  d[, lpa_vs_healthy := lpa - d$lpa[1]]
-  d[, sb_vs_healthy := sb - d$sb[1]]
+  # constrast vs healthy
+  d[, sleep_vs_healthy := sleep - d[cancer_time == "Healthy"]$sleep]
+  d[, mvpa_vs_healthy := mvpa - d[cancer_time == "Healthy"]$mvpa]
+  d[, lpa_vs_healthy := lpa - d[cancer_time == "Healthy"]$lpa]
+  d[, sb_vs_healthy := sb - d[cancer_time == "Healthy"]$sb]
   
-  # constrast vs lag group
-  d[, sleep_vs_lag := sleep - shift(sleep, type = "lag")]
-  d[, mvpa_vs_lag := mvpa - shift(mvpa, type = "lag")]
-  d[, lpa_vs_lag := lpa - shift(lpa, type = "lag")]
-  d[, sb_vs_lag := sb - shift(sb, type = "lag")]
+  # pairwise contrast within cancer 
+  d[, sleep_lessthan1_vs_1to5 := d[cancer_time == "1-5 years since diagnosis"]$sleep[1] - d[cancer_time == "Less than 1 year since diagnosis"]$sleep[1]]
+  d[, sleep_lessthan1_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$sleep[1] - d[cancer_time == "Less than 1 year since diagnosis"]$sleep[1]]
+  d[, sleep_1to5_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$sleep[1] - d[cancer_time == "1-5 years since diagnosis"]$sleep[1]]
   
+  d[, mvpa_lessthan1_vs_1to5 := d[cancer_time == "1-5 years since diagnosis"]$mvpa[1] - d[cancer_time == "Less than 1 year since diagnosis"]$mvpa[1]]
+  d[, mvpa_lessthan1_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$mvpa[1] - d[cancer_time == "Less than 1 year since diagnosis"]$mvpa[1]]
+  d[, mvpa_1to5_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$mvpa[1] - d[cancer_time == "1-5 years since diagnosis"]$mvpa[1]]
+
+  d[, lpa_lessthan1_vs_1to5 := d[cancer_time == "1-5 years since diagnosis"]$lpa[1] - d[cancer_time == "Less than 1 year since diagnosis"]$lpa[1]]
+  d[, lpa_lessthan1_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$lpa[1] - d[cancer_time == "Less than 1 year since diagnosis"]$lpa[1]]
+  d[, lpa_1to5_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$lpa[1] - d[cancer_time == "1-5 years since diagnosis"]$lpa[1]]
+
+  d[, sb_lessthan1_vs_1to5 := d[cancer_time == "1-5 years since diagnosis"]$sb[1] - d[cancer_time == "Less than 1 year since diagnosis"]$sb[1]]
+  d[, sb_lessthan1_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$sb[1] - d[cancer_time == "Less than 1 year since diagnosis"]$sb[1]]
+  d[, sb_1to5_vs_morethan5 := d[cancer_time == "More than 5 years since diagnosis"]$sb[1] - d[cancer_time == "1-5 years since diagnosis"]$sb[1]]
+
   d <- d[, .(cancer_time, 
              sleep, mvpa, lpa, sb,
+             
              sleep_vs_healthy, mvpa_vs_healthy, lpa_vs_healthy, sb_vs_healthy,
-             sleep_vs_lag, mvpa_vs_lag, lpa_vs_lag, sb_vs_lag
+             
+             sleep_lessthan1_vs_1to5, sleep_lessthan1_vs_morethan5, sleep_1to5_vs_morethan5,
+             mvpa_lessthan1_vs_1to5, mvpa_lessthan1_vs_morethan5, mvpa_1to5_vs_morethan5,
+             lpa_lessthan1_vs_1to5, lpa_lessthan1_vs_morethan5, lpa_1to5_vs_morethan5,
+             sb_lessthan1_vs_1to5, sb_lessthan1_vs_morethan5, sleep_1to5_vs_morethan5
+             
   )]
-  d <- unique(d)
+  d1 <- unique(d)
 })
 
 # assemble back to summarise posteriors
-pred <- as.data.frame(abind(pred, along = 1))
-pred <- split(pred, pred$cancer_time)
+pred_cancer_time_adj <- as.data.frame(abind(pred_cancer_time_adj, along = 1))
+pred_cancer_time_adj <- split(pred_cancer_time_adj, pred_cancer_time_adj$cancer_time)
 
 ## Est means  ----------------------
-pred_comp_cancer_time_adj <- lapply(pred, function(l) {
+pred_comp_cancer_time_adj <- lapply(pred_cancer_time_adj, function(l) {
   l <- as.data.frame(l[, c("sleep", "mvpa", "lpa", "sb")])
   l <- apply(l, 2, as.numeric)
   l <- apply(l, 2, describe_posterior, centrality = "mean")
@@ -69,7 +86,7 @@ pred_comp_cancer_time_adj <- rbindlist(pred_comp_cancer_time_adj)
 
 ## Contrasts --------------------
 ### vs healthy
-diff1_comp_cancer_time_adj <- lapply(pred, function(l) {
+diff1_comp_cancer_time_adj <- lapply(pred_cancer_time_adj, function(l) {
   l <- as.data.frame(l[, c("sleep_vs_healthy", "mvpa_vs_healthy", "lpa_vs_healthy", "sb_vs_healthy")])
   l <- apply(l, 2, as.numeric)
   l <- apply(l, 2, describe_posterior, centrality = "mean")
@@ -85,7 +102,7 @@ setnames(diff1_comp_cancer_time_adj, "CI_low", "CI_low_diff_ref_healthy")
 setnames(diff1_comp_cancer_time_adj, "CI_high", "CI_high_diff_ref_healthy")
 
 ### vs lag
-diff2_comp_cancer_time_adj <- lapply(pred, function(l) {
+diff2_comp_cancer_time_adj <- lapply(pred_cancer_time_adj, function(l) {
   l <- as.data.frame(l[, c("sleep_vs_lag", "mvpa_vs_lag", "lpa_vs_lag", "sb_vs_lag" )])
   l <- apply(l, 2, as.numeric)
   l <- apply(l, 2, describe_posterior, centrality = "mean")
@@ -159,7 +176,7 @@ comp_cancer_time_adj[, text_position := max(CI_high), by = part]
     scale_colour_manual(values = pal_time) +
     # scale_colour_jco() +
     labs(x = "", y = "", colour = "") +
-    coord_flip() +      
+    coord_flip() +
     theme_ipsum() +
     theme(
       axis.ticks          = element_blank(),
